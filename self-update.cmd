f06b3b3379410603de2fb66dca65b6050f32dc18
Kai <kaido4492@gmail.com>
2024-11-29 10:42:38 +0700

    [IMPROVEMENT] Include upgrading Warden in the `warden self-update` command

--- self-update.cmd
+++ self-update.cmd
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 [[ ! ${WARDEN_DIR} ]] && >&2 echo -e "\033[31mThis script is not intended to be run directly!\033[0m" && exit 1
 
+## Upgrade Warden to the latest version
+git -C ${WARDEN_DIR} fetch origin
+git -C ${WARDEN_DIR} reset --hard origin/main
+git -C ${WARDEN_DIR} clean -fd
+warden svc up -d --remove-orphans
+
 ## Revert local changes and pull the latest updates from remote repository
 git -C ~/.warden/commands fetch origin
 git -C ~/.warden/commands reset --hard origin/master
